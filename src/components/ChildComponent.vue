c79c27b13a54ef21b656424178e4390d7e8cab70
moumitasub <moumitasub@gmail.com>
2023-08-20 21:47:12 +0600

    initial commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps(['message'])
+defineEmits(['messageEmitted'])
+
+</script>
+
+<template>
+    <div class="mt-10 mb-10 bg-orange-300 p-4">
+        <h1 class="text-black-900 text-xl my-5">Child Component</h1>
+        <h4 class="text-white-900">{{ message }}</h4>
+
+        <button @click="$emit('messageEmitted', 'Message from Child to Parent')" class="bg-blue-500 hover:bg-blue-700 text-white font-bold my-8 py-2 px-4 rounded">
+            Button
+        </button>
+    </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+import ChildComponent from './ChildComponent.vue';
+let message =ref("Message from Parent to Child !")
+
+function updateMessage(childMessage) {
+    message.value = childMessage
+}
+</script>
+
+<template>
+     <div class="mt-10 mb-10 bg-orange-600 p-10">
+        <h1 class="text-black-900 text-xl my-5 ">Parent Component</h1>
+        <ChildComponent :message="message" @messageEmitted="updateMessage"/>
+        
+    </div>
+  
+</template>
+
+<style scoped>
+
+</style>
